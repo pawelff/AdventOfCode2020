036f081cefea24407ea04ed46c3b69729a01cb77
Pawel F <pawe.93945@gmail.com>
2020-12-09 13:10:48 +0100

    day 09 fix

--- day_09/day_09.py
+++ day_09/day_09.py
@@ -8,18 +8,17 @@ def first_invalid_number_xmas(cipher, preamble):
 			if (num - prev_num) not in cipher[i:preamble+i] and (num - prev_num) != prev_num:
 				count += 1
 		if count == preamble:
-			return id, num
+			return num
 				
 
-id, invalid = first_invalid_number_xmas(data, 25)
+invalid = first_invalid_number_xmas(data, 25)
 print(f'Parto one: {invalid}')
 
-def encryption_weakness(cipher, invalid, id):
-	cipher = cipher[:invalid]
+def encryption_weakness(cipher, invalid):
 	for i in range(2, len(cipher)):
 		for j, num in enumerate(cipher):
 			if sum(cipher[j:j+i]) == invalid:
 				return min(cipher[j:j+i]) + max(cipher[j:j+i])
 		
 		
-print(f'Part two: {encryption_weakness(data, invalid, id)}')
+print(f'Part two: {encryption_weakness(data, invalid)}')
